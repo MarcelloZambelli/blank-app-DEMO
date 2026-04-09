6a2813e5623d1ac98b0eb24fc09634cb7649afc8
MarcelloZambelli <153529034+MarcelloZambelli@users.noreply.github.com>
2026-04-09 14:14:48 +0000

    Prova

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,19 +1,10 @@
 import streamlit as st
-from custom import top_menu, bottom_head
+import pandas as gpd
+from custom2 import top_menu, bottom_head
 
 top_menu()
 
-my_sport = ["Soccer", "Tennis", "Baketball"]
-
-if st.button("Sports"):
-    st.write("My sports are", my_sport)
 
 
-check_out = st.checkbox("Do you like football?")
-
-if check_out:
-    st.write("Great")
-else:
-    st.write("What a pity!")
 
 bottom_head()
