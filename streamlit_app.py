3a077b1e4c7fa30fd04e42924b4383927b18a43b
MarcelloZambelli <153529034+MarcelloZambelli@users.noreply.github.com>
2026-04-23 08:22:58 +0000

    Bho

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,23 @@
 import streamlit as st
 import pandas as pd
-#import geopandas as gpd
 from custom import top_menu, bottom_head
+from numpy.random import default_rng as rng
+
 
 top_menu()
+
+df = pd.DataFrame(
+    [[19.64, -155.99]],
+    columns=["lat", "lon"],
+)
+
+text, map = st.columns([.35, .65])
+
+with map:
+    #st.map(df, color="#ff0000", size=2000)
+    
+
+with text:
+    st.write("Qui ci vanno le parole")
 
 bottom_head()
